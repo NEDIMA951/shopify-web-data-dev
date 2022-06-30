d86965a55af6f894e207d382a1ca84ca0ef638cf
Dima Nezdymenko <nezdymenko@etcetera.kiev.ua>
2022-06-30 20:32:27 +0300

    product bg color picker js function

--- assets/custom.js
+++ assets/custom.js
@@ -2,4 +2,20 @@
 / Custom Theme JS
 /-----------------------------------------------------------------------------*/
 
-// Insert any custom theme js here...
+// Insert any custom theme js here
+document.addEventListener("DOMContentLoaded", () => {
+	color_picker();
+  });
+
+function color_picker() {
+	let color = document.getElementById('Color');
+	let product_bg = document.getElementsByClassName('featured-product__media-column');
+	let value = color.value;
+
+	product_bg.style.backgroundColor = value;
+
+	color.addEventListener("change", function() {
+		product_bg.style.backgroundColor = value;
+	}
+
+}
